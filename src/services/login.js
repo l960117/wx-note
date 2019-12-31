c5776ea3b47ecbbdfa2f5560bd94877b7de3df0b
lws <wensheng@yougola.com>
2019-12-31 16:50:33 +0800

    'login'

--- src/services/login.js
+++ src/services/login.js
@@ -2,22 +2,16 @@ import Taro from '@tarojs/taro'
 import loginIn from './app'
 
 const login = () => {
-  Taro.checkSession({
-    success () {
-    },
-    fail () {
-      Taro.login().then(res => {
-        loginIn({code: res.code ||''})
-        .then((data) => {
-          if (data.resultCode === 200) {
-            Taro.setStorageSync('key', data.data)
-            Taro.navigateTo({
-              url: '/pages/home/index'
-            })
-          }
+  Taro.login().then(res => {
+    loginIn({code: res.code ||''})
+    .then((data) => {
+      if (data.resultCode === 200) {
+        Taro.setStorageSync('key', data.data)
+        Taro.navigateTo({
+          url: '/pages/home/index'
         })
-      })
-    }
+      }
+    })
   })
 }
 
